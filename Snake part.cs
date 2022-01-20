8c308bd2e71d0a82a8e4d3933daed91664d3c7e2
JoeSkillPlay <dans.puduls17@gmail.com>
2022-01-20 19:15:41 +0200

    Game area created

---
+++ Snake part.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+
+namespace WpfSnakeGame
+{
+    public class Snake_part
+    {
+        public UIElement UiElement { get; set; }
+
+        public Point Position { get; set; }
+
+        public bool IsHead { get; set; }
+
+
+
+    }
+}
